d89195dc2c26b61e0568eafe68ea20b5893dc8eb
Benjamin Daniel Morgan <benjamindanielmorgan@benjamins-mbp.home>
2020-11-24 14:54:23 +0000

    first commit

---
+++ 2.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _2._1
+{
+    // author: Benjamin Daniel Morgan
+    // date: 11/12/12
+
+    class Program
+    {
+        static void Main()
+        {
+            string input;
+            int Teams, Players, Size, Remainder;
+
+            Console.Clear();
+            Console.WriteLine("How many players are there?");
+            input = Console.ReadLine();
+            Players = Convert.ToInt32(input);
+
+            Console.WriteLine("How many players are in each team?");
+            input = Console.ReadLine();
+            Size = Convert.ToInt32(input);
+
+            Teams = Players / Size;
+            Remainder = Players % Size;
+
+            Console.WriteLine("There are\t"+ Teams +"\tteams and there are\t"+ Remainder +"\tplayer(s) left over!");
+        }
+    }
+}
